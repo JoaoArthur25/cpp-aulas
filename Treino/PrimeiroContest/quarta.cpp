edf1057562098cfa158e3d6504fa8e313ba96747
JoaoArthur25 <jarthurlsilva25@gmail.com>
2025-04-15 18:01:32 -0300

    Quarta do contest

---
+++ Treino/PrimeiroContest/quarta.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> a(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    reverse(a.begin(), a.end());
+
+    while(q--){
+        int x, y;
+
+        cin >> x >> y;
+
+        int resposta = 0;
+
+        for(i = x - y; i < x; i++){
+            resposta += a[i];
+        }
+        cout << resposta << endl;
+    }
+    
+
+    return 0;
+}
